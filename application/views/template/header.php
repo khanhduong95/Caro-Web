69d69760d4dcfafd04b182d7d9b8e9ad185807b0
khanhduong95 <khanhduongdv@gmail.com>
2017-02-03 10:48:47 +0700

    remove unnecessary folders + update installation note

--- application/views/template/header.php
+++
@@ -1,7 +0,0 @@
-<?php
-defined('BASEPATH') OR exit('No direct script access allowed');
-?>
-<!DOCTYPE html>
-<script type="text/javascript" src="<?php echo base_url();?>assets/js/jquery-3.1.1.min.js"></script>
-<script src="<?php echo base_url();?>assets/js/jquery-ui-1.12.1/jquery-ui.js"></script>
-<script src="<?php echo base_url();?>assets/js/jquery-ui-1.12.1/jquery-ui.min.js"></script>
