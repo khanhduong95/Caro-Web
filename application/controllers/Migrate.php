4f239c430e2e74775a0de58ba9a724ff05b871e3
khanhduong95 <khanhduongdv@gmail.com>
2017-02-20 00:06:08 +0700

    update license notice to config files

--- application/controllers/Migrate.php
+++ application/controllers/Migrate.php
@@ -1,4 +1,23 @@
-<?php  if ( ! defined('BASEPATH')) exit("No direct script access allowed");
+<?php 
+/*
+ *   Copyright (C) 2016 Dang Duong
+ *
+ *   This file is part of Free Caro Online.
+ *
+ *   Free Caro Online is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU Affero General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   Free Caro Online is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU Affero General Public License for more details.
+ *
+ *   You should have received a copy of the GNU Affero General Public License
+ *   along with Free Caro Online.  If not, see <http://www.gnu.org/licenses/>.
+ */
+if ( ! defined('BASEPATH')) exit("No direct script access allowed");
 
 class Migrate extends CI_Controller {
 
